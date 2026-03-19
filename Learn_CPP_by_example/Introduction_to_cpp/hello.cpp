6ce35b9fef9588d9a4e54427c7e1fafcda70f476
George Calin <george.calin@gmail.com>
2026-03-19 15:43:10 +0200

    Add hello.cpp example demonstrating basic input and output in C++

---
+++ Learn_CPP_by_example/Introduction_to_cpp/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello from C++"<<endl;
+    int my_number;
+
+    cin>>my_number;
+    cout<<"The number you entered is "<<my_number<<endl;
+
+    return 0;
+}
