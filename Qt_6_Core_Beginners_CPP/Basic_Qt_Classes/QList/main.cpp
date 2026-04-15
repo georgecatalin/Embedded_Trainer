801a8d9ba884c4a6fb1cc4221623237660d3d0d9
George Calin <george.calin@gmail.com>
2026-04-15 12:59:47 +0300

    Qlist<T>

---
+++ Qt_6_Core_Beginners_CPP/Basic_Qt_Classes/QList/main.cpp
@@ -0,0 +1,77 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString phrase = "Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals."
+                     " Set time aside to learn and get reminders using your learning scheduler.";
+    QList<QString> list = phrase.split(" ");
+
+    foreach (QString var, list)
+    {
+        qInfo()<<var;
+    }
+
+    QList<int> listInts({1977,1978,2011,2020,1954,1953});
+
+    foreach (int var, listInts)
+    {
+        qInfo()<<var;
+    }
+
+    listInts.append(2);
+    qInfo()<<listInts;
+
+    listInts.remove(0);
+    qInfo()<<listInts;
+
+    return QCoreApplication::exec();
+}
+
+/*
+"Learning"
+    "a"
+    "little"
+    "each"
+    "day"
+    "adds"
+    "up."
+    "Research"
+    "shows"
+    "that"
+    "students"
+    "who"
+    "make"
+    "learning"
+    "a"
+    "habit"
+    "are"
+    "more"
+    "likely"
+    "to"
+    "reach"
+    "their"
+    "goals."
+    "Set"
+    "time"
+    "aside"
+    "to"
+    "learn"
+    "and"
+    "get"
+    "reminders"
+    "using"
+    "your"
+    "learning"
+    "scheduler."
+    1977
+    1978
+    2011
+    2020
+    1954
+    1953
+    QList(1977, 1978, 2011, 2020, 1954, 1953, 2)
+    QList(1978, 2011, 2020, 1954, 1953, 2)
+    */
